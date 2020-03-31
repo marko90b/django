084f399e9425b670005ad9f468bc0648bb7b67ce
marko90b <58029074+marko90b@users.noreply.github.com>
2020-03-31 07:39:43 +0200

    Update models.py

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -60,26 +60,32 @@ class Permission(models.Model):
         verbose_name=_('content type'),
     )
     codename = models.CharField(_('codename'), max_length=100)
+    department = models.ForeignKey(
+        'users.Department',
+        models.CASCADE,
+    )
 
     objects = PermissionManager()
 
     class Meta:
         verbose_name = _('permission')
         verbose_name_plural = _('permissions')
-        unique_together = (('content_type', 'codename'),)
-        ordering = ('content_type__app_label', 'content_type__model',
-                    'codename')
+        unique_together = (('content_type', 'codename', 'department'),)
+        ordering = ('department', 'content_type__app_label',
+                    'content_type__model', 'codename')
 
     def __str__(self):
-        return "%s | %s | %s" % (
+        return "%s | %s | %s | %s" % (
+            self.department.name,
             self.content_type.app_label,
             self.content_type,
             self.name,
         )
 
     def natural_key(self):
-        return (self.codename,) + self.content_type.natural_key()
-    natural_key.dependencies = ['contenttypes.contenttype']
+        return (self.department.name, self.codename,
+                        ) + self.content_type.natural_key()
+    natural_key.dependencies = ['contenttypes.contenttype'] 
 
 
 class GroupManager(models.Manager):
